461f657f289a3b324a8915fbdb9890ad6153a24c
long.nguyenduc <long.nguyenduc@ncc.asia>
2025-04-23 16:28:29 +0700

    feature/#1-set-up-base-for-logic

--- src/bot/commands/asterisk/apply-cv/apply-cv.command.ts
+++ src/bot/commands/asterisk/apply-cv/apply-cv.command.ts
@@ -5,7 +5,11 @@ import {
 } from 'src/bot/commands/asterisk/config/config';
 import { COLORS } from 'src/bot/utils/helper';
 import { MezonClientConfig } from 'src/mezon/dtos/mezon-client-config';
-import { EMessageComponentType } from 'mezon-sdk';
+import {
+  ChannelMessage,
+  EButtonMessageStyle,
+  EMessageComponentType,
+} from 'mezon-sdk';
 import {
   branchOptions,
   candidateTypesOptions,
@@ -13,166 +17,217 @@ import {
   genderOptions,
   positionOptions,
 } from './apply-cv.constant';
+import { CommandMessage } from 'src/bot/abstractions/commands/asterisk.abstract';
 
 @Command('apply-cv')
-export class ApplyCVCommand {
-  constructor(private readonly clientConfigService: MezonClientConfig) {}
+export class ApplyCVCommand extends CommandMessage {
+  constructor(private readonly clientConfigService: MezonClientConfig) {
+    super();
+  }
 
-  embed: EmbedProps[] = [
-    {
-      color: COLORS.Aqua,
-      title: `Candidate Application Form`,
-      fields: [
+  execute(args: string | boolean | any[] | string[], message: ChannelMessage) {
+    const attachments = message.attachments;
+    const isCorrectFileType = attachments.some(
+      (a) => a.filetype === 'pdf' || a.filetype === 'docx',
+    );
+    if (!isCorrectFileType) {
+      return this.generateReplyMessage(
         {
-          name: 'Full Name*',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-fullname`,
-            type: EMessageComponentType.INPUT,
-            component: {
-              id: `applycv-${messageid}-fullname-plhder`,
-              placeholder: 'Enter full name',
-              required: true,
+          content: `Chỉ chấp nhận định dạng PDF hoặc DOCX.`,
+        },
+        message,
+      );
+    }
+    const messageid = message.id;
+
+    const embed: EmbedProps[] = [
+      {
+        color: COLORS.Aqua,
+        title: `Candidate Application Form`,
+        fields: [
+          {
+            name: 'Full Name*',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-fullname`,
+              type: EMessageComponentType.INPUT,
+              component: {
+                id: `applycv-${messageid}-fullname-plhder`,
+                placeholder: 'Enter full name',
+                required: true,
+              },
             },
           },
-        },
-        {
-          name: 'Email*',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-email`,
-            type: EMessageComponentType.INPUT,
-            component: {
-              id: `applycv-${messageid}-email-plhder`,
-              placeholder: 'example@example.com',
-              required: true,
-              type: 'email',
+          {
+            name: 'Email*',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-email`,
+              type: EMessageComponentType.INPUT,
+              component: {
+                id: `applycv-${messageid}-email-plhder`,
+                placeholder: 'example@example.com',
+                required: true,
+                type: 'email',
+              },
             },
           },
-        },
-        {
-          name: 'Phone Number*',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-phone`,
-            type: EMessageComponentType.INPUT,
-            component: {
-              id: `applycv-${messageid}-phone-plhder`,
-              placeholder: 'Enter phone number',
-              required: true,
-              type: 'text',
+          {
+            name: 'Phone Number*',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-phone`,
+              type: EMessageComponentType.INPUT,
+              component: {
+                id: `applycv-${messageid}-phone-plhder`,
+                placeholder: 'Enter phone number',
+                required: true,
+                type: 'text',
+              },
             },
           },
-        },
-        {
-          name: 'Candidate Type*',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-candidate-type`,
-            type: EMessageComponentType.SELECT,
-            component: {
-              options: candidateTypesOptions,
-              required: true,
-              valueSelected: candidateTypesOptions[0],
+          {
+            name: 'Candidate Type*',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-candidate-type`,
+              type: EMessageComponentType.SELECT,
+              component: {
+                options: candidateTypesOptions,
+                required: true,
+                valueSelected: candidateTypesOptions[0],
+              },
             },
           },
-        },
-        {
-          name: 'Position*',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-position`,
-            type: EMessageComponentType.SELECT,
-            component: {
-              options: positionOptions,
-              required: true,
-              valueSelected: positionOptions[0],
+          {
+            name: 'Position*',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-position`,
+              type: EMessageComponentType.SELECT,
+              component: {
+                options: positionOptions,
+                required: true,
+                valueSelected: positionOptions[0],
+              },
             },
           },
-        },
-        {
-          name: 'Branch*',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-branch`,
-            type: EMessageComponentType.SELECT,
-            component: {
-              options: branchOptions,
-              required: true,
-              valueSelected: branchOptions[0],
+          {
+            name: 'Branch*',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-branch`,
+              type: EMessageComponentType.SELECT,
+              component: {
+                options: branchOptions,
+                required: true,
+                valueSelected: branchOptions[0],
+              },
             },
           },
-        },
-        {
-          name: 'CV Source*',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-cv-source`,
-            type: EMessageComponentType.SELECT,
-            component: {
-              options: cvSourceOptions,
-              required: true,
-              valueSelected: cvSourceOptions[0],
+          {
+            name: 'CV Source*',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-cv-source`,
+              type: EMessageComponentType.SELECT,
+              component: {
+                options: cvSourceOptions,
+                required: true,
+                valueSelected: cvSourceOptions[0],
+              },
             },
           },
-        },
-        {
-          name: 'Date of birth',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-dob`,
-            type: EMessageComponentType.INPUT,
-            component: {
-              id: `applycv-${messageid}-dob-plhder`,
-              placeholder: 'DD/MM/YYYY',
-              required: false,
-              type: 'date',
+          {
+            name: 'Date of birth',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-dob`,
+              type: EMessageComponentType.INPUT,
+              component: {
+                id: `applycv-${messageid}-dob-plhder`,
+                placeholder: 'DD/MM/YYYY',
+                required: false,
+                type: 'date',
+              },
             },
           },
-        },
-        {
-          name: 'Gender',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-gender`,
-            type: EMessageComponentType.SELECT,
-            component: {
-              options: genderOptions,
-              required: false,
-              valueSelected: genderOptions[0],
+          {
+            name: 'Gender',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-gender`,
+              type: EMessageComponentType.SELECT,
+              component: {
+                options: genderOptions,
+                required: false,
+                valueSelected: genderOptions[0],
+              },
             },
           },
-        },
-        {
-          name: 'Address',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-address`,
-            type: EMessageComponentType.INPUT,
-            component: {
-              id: `applycv-${messageid}-address-plhder`,
-              placeholder: 'Enter address',
-              required: false,
+          {
+            name: 'Address',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-address`,
+              type: EMessageComponentType.INPUT,
+              component: {
+                id: `applycv-${messageid}-address-plhder`,
+                placeholder: 'Enter address',
+                required: false,
+              },
             },
           },
-        },
-        {
-          name: 'Note',
-          value: '',
-          inputs: {
-            id: `applycv-${messageid}-note`,
-            type: EMessageComponentType.INPUT,
-            component: {
-              id: `applycv-${messageid}-note-plhder`,
-              placeholder: 'Additional information',
-              required: false,
-              textarea: true,
+          {
+            name: 'Note',
+            value: '',
+            inputs: {
+              id: `applycv-${messageid}-note`,
+              type: EMessageComponentType.INPUT,
+              component: {
+                id: `applycv-${messageid}-note-plhder`,
+                placeholder: 'Additional information',
+                required: false,
+                textarea: true,
+              },
             },
           },
+        ],
+        timestamp: new Date().toISOString(),
+        footer: MEZON_EMBED_FOOTER,
+      },
+    ];
+
+    const components = [
+      {
+        components: [
+          {
+            id: `CV_${messageid}_cancel`,
+            type: EMessageComponentType.BUTTON,
+            component: {
+              label: `Cancel`,
+              style: EButtonMessageStyle.SECONDARY,
+            },
+          },
+          {
+            id: `CV_${messageid}_submit`,
+            type: EMessageComponentType.BUTTON,
+            component: {
+              label: `Submit`,
+              style: EButtonMessageStyle.SUCCESS,
+            },
+          },
+        ],
+      },
+    ];
+    if (isCorrectFileType) {
+      return this.generateReplyMessage(
+        {
+          embed,
+          components,
         },
-      ],
-      timestamp: new Date().toISOString(),
-      footer: MEZON_EMBED_FOOTER,
-    },
-  ];
+        message,
+      );
+    }
+  }
 }
